9871c596b612dede08e719a49bb9b71f9e5dbc8e
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2025-04-10 15:19:44 -0300

    Create contest-0-C-Joao-Duarte-Colares.cpp

---
+++ contest-0-C-Joao-Duarte-Colares.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+    int a; 
+    cin>> a;
+   
+    if(a%2==0 and a!= 1){
+        cout<<"YES"<<"\n";
+    }else{
+        cout<<"NO"<<"\n";
+    }
+
+}
