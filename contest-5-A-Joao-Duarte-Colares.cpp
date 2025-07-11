9f204f9ef4144b71917698e15dde6703955fc4be
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2025-07-11 16:52:03 -0300

    Update contest-5-A-Joao-Duarte-Colares.cpp

--- contest-5-A-Joao-Duarte-Colares.cpp
+++ contest-5-A-Joao-Duarte-Colares.cpp
@@ -15,6 +15,30 @@ public:
 };
 
 
+bool consulta(vector<bool> mais_que_o_limite, int number){
+    
+    if(mais_que_o_limite[number]){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+char int_to_char(int a){
+    if(a==0){return '0';}
+    else if(a==1){return '1';}
+    else if(a==2){return '2';}
+    else if(a==3){return '3';}
+    else if(a==4){return '4';}
+    else if(a==5){return '5';}
+    else if(a==6){return '6';}
+    else if(a==7){return '7';}
+    else if(a==8){return '8';}
+    else if(a==9){return '9';}
+}
+
 int resultado(Number entrada){
 
     /*
@@ -75,12 +99,69 @@ int resultado(Number entrada){
     for(int i = 0; i< 10; i++){
       if(entrada.quantidade_cada[i] > 2 ){
         mais_que_o_limite[i] = true;
-        cout<<"o algarismo "<<i<<" é maior que o máximo"<<endl;
+        
       }
     }
-
+    
+    for(int i = 0; i<10;i++){
+        if(consulta(mais_que_o_limite,i)){
+            cout<<"o algarismo "<<i<<" é maior que o máximo"<<endl;
+        }
+    }
+    
+    
+    //percorrendo a partir do início, se um algarismo é igual a um número que é TRUE em mais_que_limite[o número], então esse
+    //número necessariamente deve mudar, se for o terceiro encontrado. (manter lista de encontrados de cada valor de 0 a 9)
+    
+    int encontrados[10] = {0,0,0,0,0,0,0,0,0,0};
     
 
+
+    
+    //quando mudamos um valor diferente de zero,modificamos quantidade_cada[valor] para quantidade_cada[valor]-1 
+    //e modificamos mais_que_limite[valor] caso quantidade_cada[valor] fique menor que 3
+    for(int i =0; i < entrada.valor_string.length();i++){
+        
+        
+        int auxiliar = entrada.valor_string[i] - '0';
+        
+        //caso o algarismo não seja 0
+        if(auxiliar != 0){    
+            if(entrada.quantidade_cada[auxiliar] > 2){
+                cout<<"o valor na posição "<< i <<"é maior que o limite!"<<endl;
+                encontrados[auxiliar]++;
+                
+                //se for o terceiro então é ele que devemos subtrair 1
+                if(encontrados[auxiliar] == 3){
+                    
+                    
+                    char a = int_to_char(auxiliar-1);
+                    entrada.valor_string[i] = a;
+                    
+                    //atualizo os dados do valor subtraído
+                    encontrados[auxiliar]--;
+                    entrada.quantidade_cada[auxiliar]--;
+                    
+                    //atualizo os dados da posição após subtração
+                    entrada.quantidade_cada[auxiliar-1]++;
+                    
+                    //repete enquanto precisar
+                    if(entrada.valor_string[i] > 0){i--;}
+                    
+                }
+            }
+        }
+        
+    }
+    
+    cout<<"depois de mudar: "<<endl;
+    
+    cout<<entrada.valor_string<<endl;
+    
+
+    
+    
+    
 
 
 }
