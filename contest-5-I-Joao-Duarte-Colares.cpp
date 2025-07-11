0cb72f82c0938d63d259588057e5027a4d52e33d
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2025-07-11 15:34:38 -0300

    Update contest-5-I-Joao-Duarte-Colares.cpp

--- contest-5-I-Joao-Duarte-Colares.cpp
+++ contest-5-I-Joao-Duarte-Colares.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//https://vjudge.net/contest/728955#problem/I
+
 int main(){
 
 //valor de 1 a 4  
@@ -11,21 +13,21 @@ int tea_type;
 //chutes de cada participante. varia entre 1 e 4  
 int chutes[5];
 
-cin>>tea_type;
-
-for(int i = 0; i< 5;i++){
-  cin>>chutes[i];
-  
+while(cin>>tea_type){
+    
+    for(int i = 0; i< 5;i++){
+      cin>>chutes[i];
+      
+    }
+    
+    int resultado = 0;
+    
+    for(int i = 0; i<5;i++){
+      if(chutes[i] == tea_type){resultado++;}
+    }
+    
+    cout<<resultado<<endl;  
 }
-
-cout<<tea_type<<endl;
-
-for(int i = 0; i<5;i++){
-  cout<<" "<<chutes[i]<<" ";
-}
-  
-
-  
 
 return 0;
 }
